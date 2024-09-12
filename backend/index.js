0ff5c65aedc990fea1e04858e8e23979375b85e3
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-09-12 16:32:32 +0530

    Add this file

--- backend/index.js
+++ backend/index.js
@@ -30,37 +30,42 @@ app.get("/", (req, res) => {
 app.get("/payment", async (req, res) => {
   try {
     let request = {
-      "order_amount": 1.0,
-      "order_currency": "INR",
-      "order_id": await generatOrderId(),
-      "customer_details": {
-        "customer_id": "nitin193949",
-        "customer_phone": "9999999999",
-        "customer_name": "nitin",
-        "customer_email": "nitinkondhari85@gmail.com",
+      order_amount: 200.0,
+      order_currency: "INR",
+      order_id: await generatOrderId(),
+      customer_details: {
+        customer_id: "nitin193949",
+        customer_phone: "9999999999",
+        customer_name: "nitin",
+        customer_email: "nitinkondhari85@gmail.com",
       },
     };
-    Cashfree.PGCreateOrder("2023-08-01", request).then((response) => {
-     console.log(response.data);
-      res.json(response.data)
-    }).catch(error=>{
-      console.log(error.response.data.message)
-    });
+    Cashfree.PGCreateOrder("2023-08-01", request)
+      .then((response) => {
+        console.log(response.data);
+        res.json(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
   } catch (error) {
     console.log(error);
   }
 });
 app.post("/verify", (req, res) => {
-      try {
-            let {orderId}=req.body;
-            Cashfree.PGOrderFetchPayments("2023-08-01",orderId).then((response)=>{
-              res.json(response.data)
-            }).catch((error)=>{
-                  console.log(error)
-            })
-      } catch (error) {
-           console.log(error) 
-      }
+  try {
+    let { orderId } = req.body;
+    Cashfree.PGOrderFetchPayments("2023-08-01", orderId)
+      .then((response) => {
+        res.json(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
 });
 app.listen(8080, () => {
   console.log("Server run posrt at 8080");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,36 +29,54 @@ function App() {
         paymentSessionId: sessionId,
         redirectTarget: "_modal",
       };
+      console.log(checkoutOptions);
       cashfree.checkout(checkoutOptions).then((result) => {
         if (result.error) {
-          // This will be true whenever user clicks on close icon inside the modal or any error happens during the payment
           console.log(
             "User has closed the popup or there is some payment error, Check for Payment Status"
           );
           console.log(result.error);
         }
         if (result.redirect) {
-          // This will be true when the payment redirection page couldnt be opened in the same window
-          // This is an exceptional case only when the page is opened inside an inAppBrowser
-          // In this case the customer will be redirected to return url once payment is completed
           console.log("Payment will be redirected");
         }
         if (result.paymentDetails) {
-          // This will be called whenever the payment is completed irrespective of transaction status
           console.log("Payment has been completed, Check for Payment Status");
           console.log(result.paymentDetails.paymentMessage);
+          console.log(result);
         }
+        console.log(result);
       });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handverify = async (event) => {
+    event.preventDefault();
+    let orderId="bebead51e22f";
+    try {
+      const dataResponse = await fetch("http://localhost:8080/verify", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ orderId }),
+      });
+
+      const dataApi = await dataResponse.json();
+
+      console.log(dataApi);
     } catch (error) {
       console.log(error);
     }
   };
   return (
     <>
-      <h1>Cashfree Payment  integration</h1>
+      <h1>Cashfree Payment integration</h1>
       <div className="card">
         <button onClick={handclick}>Pay</button>
       </div>
+      <button onClick={handverify}>Verify</button>
     </>
   );
 }
